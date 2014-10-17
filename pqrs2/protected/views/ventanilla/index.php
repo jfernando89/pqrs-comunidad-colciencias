c3adf4aad79faa8f27599cdfb99d1489e439cf3b
Jaime Amaya <jamesfercho89@hotmail.com>
2014-10-17 11:29:35 -0500

    Nueva Actualización del Usuario Ventanilla

---
+++ pqrs2/protected/views/ventanilla/index.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Bienvenido Usuario Ventanillita</h1>
+
+<p>Aqu&iacute; encontrar&aacute; todas las opciones necesarias para la Gesti&oacute;n de las PQRS (Peticiones, Quejas, Reclamos y Solicitudes).</p>
+
+
+
+

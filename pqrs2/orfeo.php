219e4518b71cd05c8730ace319128db04b556a3f
William Quiceno <wi999iam@hotmail.com>
2014-11-10 17:20:34 -0500

    Página de Simulación ORFEO

--- pqrs2/orfeo.php
+++ pqrs2/orfeo.php
@@ -1,5 +1,16 @@
-<h1>Orfeo</h1>
+<html>
+<head>
+	<title>.:Orfeo:.</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
 
-<a href="index.php?r=Ventanilla">Usuario Ventanilla</a>
-<br>
-<a href="index.php?r=GAC">Usuario GAC</a>
+	<div class="container">
+		<div class="buttons">
+			<a href="index.php?r=Ventanilla">Usuario Ventanilla</a>
+			<a href="index.php?r=GAC">Usuario GAC</a>
+		</div>
+	</div>	
+
+</body>
+

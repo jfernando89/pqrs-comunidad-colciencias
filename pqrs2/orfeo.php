3384c59013dc00089ed2c70ce1fd0a1c6f8df039
William Quiceno <wi999iam@hotmail.com>
2014-11-15 12:01:19 -0500

    Actualización Github Prueba

--- pqrs2/orfeo.php
+++ pqrs2/orfeo.php
@@ -4,13 +4,11 @@
 	<link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-
 	<div class="container">
 		<div class="buttons">
 			<a href="index.php?r=Ventanilla">Usuario Ventanilla</a>
 			<a href="index.php?r=GAC">Usuario GAC</a>
 		</div>
 	</div>	
-
 </body>
 
